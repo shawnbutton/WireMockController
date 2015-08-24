9e134d45e8a79cb86a598f89f2ae06c47ce5840c
Shawn Button <shawn.button@capitalone.com>
2015-08-24 16:24:27 -0400

    tidy the test up a bit

--- spec/wire_mock_caller_spec.rb
+++ spec/wire_mock_caller_spec.rb
@@ -4,21 +4,24 @@ require 'spec_helper'
 describe 'WireMock Integration Tests - Require running WireMock instance', :integration do
 
   it 'should allow you to specify mapping and get it back' do
-    body = 'this is the body that will be returned by the call to wiremock'
 
     wiremock = WireMockCaller.new
 
     wiremock.reset_mappings
 
-    wiremock.create_mapping('/someurl', 'GET', body)
+    body = 'this is the body that will be returned by the call to wiremock'
+    get_method = 'GET'
+    url = '/someurl'
+
+    wiremock.create_mapping(url, get_method, body)
 
     mappings = wiremock.get_mappings
 
     first_mapping = mappings[0]
 
     request = first_mapping['request']
-    expect(request['url']).to eq('/someurl')
-    expect(request['method']).to eq('GET')
+    expect(request['url']).to eq(url)
+    expect(request['method']).to eq(get_method)
 
     response = first_mapping['response']
     expect(response['status']).to eq(200)
