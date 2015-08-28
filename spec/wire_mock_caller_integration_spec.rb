24e1e470e97044e1b8859980e753410e3fcf073e
Shawn Button <shawn.button@capitalone.com>
2015-08-28 11:54:52 -0400

    move integration tests to own spec

---
+++ spec/wire_mock_caller_integration_spec.rb
@@ -0,0 +1,61 @@
+require_relative '../lib/wire_mock_caller'
+require 'spec_helper'
+
+describe 'WireMock Integration Tests - Require running WireMock instance', :integration do
+
+  before :each do
+    @sut = WireMockCaller.new
+    @sut.reset_mappings
+  end
+
+  after :each do
+    @sut.reset_mappings
+  end
+
+  it 'should allow you to specify mapping and get it back' do
+
+    body = 'this is the body that will be returned by the call to wiremock'
+    get_method = 'GET'
+    url = '/someurl'
+
+    @sut.create_mapping(url, get_method, body)
+
+    mappings = @sut.get_mappings
+
+    first_mapping = mappings[0]
+
+    request = first_mapping['request']
+    expect(request['url']).to eq(url)
+    expect(request['method']).to eq(get_method)
+
+    response = first_mapping['response']
+    expect(response['status']).to eq(200)
+    expect(response['body']).to eq(body)
+
+  end
+
+  it 'should reset all mappings to default' do
+    @sut.create_mapping("someurl", "GET", "some body")
+
+    @sut.reset_mappings
+
+    mappings = @sut.get_mappings
+
+    default_mapping = '/api/json?pretty=true'
+    expect(mappings[0]['request']['url']).to eq(default_mapping)
+
+  end
+
+  it 'should throw exception when you specify invalid wiremock uri' do
+
+    @sut = WireMockCaller.new('http://someinvaliduri/')
+
+    @sut.get_mappings
+
+    default_mapping = '/api/json?pretty=true'
+    expect(mappings[0]['request']['url']).to eq(default_mapping)
+
+  end
+
+end
+
--- spec/wire_mock_caller_spec.rb
+++ spec/wire_mock_caller_spec.rb
@@ -1,51 +1,21 @@
 require_relative '../lib/wire_mock_caller'
 require 'spec_helper'
 
-describe 'WireMock Integration Tests - Require running WireMock instance', :integration do
+describe 'WireMock Caller Tests' do
 
   before :each do
     @sut = WireMockCaller.new
-    @sut.reset_mappings
   end
 
-  after :each do
-    @sut.reset_mappings
-  end
+  it 'should throw exception when you specify invalid wiremock uri' do
 
-  it 'should allow you to specify mapping and get it back' do
+    pending("code is not implemented")
 
-    body = 'this is the body that will be returned by the call to wiremock'
-    get_method = 'GET'
-    url = '/someurl'
+    @sut = WireMockCaller.new('http://someinvaliduri/')
 
-    @sut.create_mapping(url, get_method, body)
-
-    mappings = @sut.get_mappings
-
-    first_mapping = mappings[0]
-
-    request = first_mapping['request']
-    expect(request['url']).to eq(url)
-    expect(request['method']).to eq(get_method)
-
-    response = first_mapping['response']
-    expect(response['status']).to eq(200)
-    expect(response['body']).to eq(body)
+    expect { @sut.get_mappings }.to raise_exception
 
   end
-
-  it 'should reset all mappings to default' do
-    @sut.create_mapping("someurl", "GET", "some body")
-
-    @sut.reset_mappings
-
-    mappings = @sut.get_mappings
-
-    default_mapping = '/api/json?pretty=true'
-    expect(mappings[0]['request']['url']).to eq(default_mapping)
-
-  end
-
 
 end
 
